9ec408bdc03cec8c327a50e3892de70e498c66fa
FennisMonjaa <160559540+Dominikyayo@users.noreply.github.com>
2024-08-16 15:46:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ document.getElementById("btnClick").addEventListener("click", function() {
     const video = document.querySelector('video');
     video.play();
 
-    // Adicionar um ouvinte de evento para detectar o término da reprodução do vídeo
     video.addEventListener('ended', function() {
         document.getElementById("btnClick").style.display = "block";
         document.getElementById("videoContainer").style.display = "none";
-        video.pause(); // Pausa o vídeo
-        video.currentTime = 0; // Reinicia o vídeo para o início
+        video.pause(); 
+        video.currentTime = 0;
     });
 });
 
